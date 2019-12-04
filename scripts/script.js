ee832e2cfcf03771ba41424769dbd152f4661e56
Michael <muwa.rezan@gmail.com>
2019-12-04 22:43:18 +0300

    Task #10

--- scripts/script.js
+++ scripts/script.js
@@ -32,4 +32,53 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    let deadLine = '2019-12-05';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor(t / 1000 / 60 / 60);
+
+            if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            if (hours < 10) {
+                hours = '0' + hours;
+            }
+        
+            return {
+                'total' : t,
+                'seconds' : seconds,
+                'minutes' : minutes,
+                'hours' : hours
+            };
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateTime, 1000);
+
+        function updateTime() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.totsl <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock('timer', deadLine);
+
 });
